a9545f8806fe0033caffd2458b218ce1d31fc8ae
karthik-govindappa <ksgovindappa@gmail.com>
2018-03-19 22:52:45 +0100

    Add docker config files

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,14 @@
 # services/users/project/__init__.py
 
+import os
+import sys
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
 @app.route('/users/ping', methods=['GET'])
