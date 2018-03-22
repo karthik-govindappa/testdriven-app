fe94239a8bea0a8ba020a0816af68227db1bfd6e
karthik-govindappa <ksgovindappa@gmail.com>
2018-03-22 06:54:48 +0100

    Added code coverage (coverage) and code quality (flake8)

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app, db
@@ -9,6 +10,16 @@ from project.api.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 
 @cli.command()
 def recreate_db():
@@ -31,6 +42,21 @@ def seed_db():
     db.session.add(User(username='michael', email='hermanmu@gmail.com'))
     db.session.add(User(username='michaelherman', email='michael@mherman.org'))
     db.session.commit()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__=='__main__':
     cli()
