49a75583665f6ded9caed7aeeba242966754f36f
Karthik <ksgovindappa@gmail.com>
2019-08-29 06:03:15 +0200

    Remove v1 related files; Initial commit for v2.3

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,26 +1,22 @@
 # services/users/project/config.py
 
-import os
-
 
 class BaseConfig:
-    """Base configuration"""
+    """
+    Base configuration
+    """
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
-    """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    """
+    Development configuration
+    """
+    pass
 
 
 class TestingConfig(BaseConfig):
-    """Testing configuration"""
+    """
+    Testing configuration
+    """
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
-
-
-class ProductionConfig(BaseConfig):
-    """Producation configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
